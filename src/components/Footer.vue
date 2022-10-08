3dc5011a31c42ee3760fb2474a00b5ad82edb665
ghuseynova <ghuseynova.dev@gmail.com>
2022-10-08 16:55:30 +0400

    thelast

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,6 +15,9 @@
                         <v-row dense no-gutters>
                             <v-col id="singer-style" @click="true">Ariana Grande</v-col>
                         </v-row>
+                        <v-row dense no-gutters>
+                            <v-col id="singer-style" @click="true">Ariana Grande</v-col>
+                        </v-row>
                     </v-col>
                     <v-col cols="auto" align-self="center" id="heart-icon">
                         <v-icon small color="rgba(245, 245, 245, 0.658)" @click="heartToggle" :style="{ color: heartToggle, red: 'white' }"
